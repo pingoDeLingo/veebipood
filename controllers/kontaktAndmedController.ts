2dba593476d9d3f9dc9248c1ca01d5d09d5aca2f
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-05-23 11:07:30 +0300

    Update kontaktAndmedController.ts

--- controllers/kontaktAndmedController.ts
+++ controllers/kontaktAndmedController.ts
@@ -1,7 +1,5 @@
 import { Router, Request, Response } from 'express';
 import KontaktAndmed from "../models/kontaktAndmed";
-import Maksestaatus from "../models/maksestaatus";
-import Kategooria from "../models/kategooria";
 
 const router: Router = Router();
 
